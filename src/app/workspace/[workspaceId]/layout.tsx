1ff02ca80f19ab15f5612c4d7ff6580dfac99e07
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-02-27 17:46:37 +0300

    install react resizable panel

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,21 +1,33 @@
 "use client";
-
 import { Toolbar } from "@/app/workspace/[workspaceId]/toolbar";
 import { Sidebar } from "@/app/workspace/[workspaceId]/sidebar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 interface WorkspaceLayoutProps {
   children: React.ReactNode;
 }
-
 const WorkspaceLayout = ({ children }: WorkspaceLayoutProps) => {
   return (
     <div className="h-full ">
-      <Toolbar />
+      {" "}
+      <Toolbar />{" "}
       <div className="flex h-[calc(100vh-40px)]">
-        <Sidebar />
-        {children}
-      </div>
+        {" "}
+        <Sidebar />{" "}
+        <ResizablePanelGroup direction="horizontal">
+          {" "}
+          <ResizablePanel>
+            {" "}
+            <div>Channels Sidebar</div>{" "}
+          </ResizablePanel>{" "}
+          <ResizableHandle withHandle />{" "}
+          <ResizablePanel>{children}</ResizablePanel>{" "}
+        </ResizablePanelGroup>{" "}
+      </div>{" "}
     </div>
   );
 };
-
 export default WorkspaceLayout;
