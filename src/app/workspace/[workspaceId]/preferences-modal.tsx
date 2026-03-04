0fab8aaf5ff1c2981c7709c36cf2eb7b3a140df8
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-04 11:55:49 +0300

    workspaces profiles

---
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TrashIcon } from "lucide-react";
+import { useUpdateWorkspace } from "@/features/workspaces/api/use-update-workspace";
+import { useRemoveWorkspace } from "@/features/workspaces/api/use-remove-workspace";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-worksapce-id";
+import { toast } from "sonner";
+
+interface PreferencesModalProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  initialValue: string;
+}
+
+export const PreferencesModal = ({
+  open,
+  setOpen,
+  initialValue,
+}: PreferencesModalProps) => {
+  const workspaceId = useWorkspaceId();
+  const [value, setValue] = useState(initialValue);
+  const [editOpen, setEditOpen] = useState(false);
+
+  const { mutate: updateWorkSpace, isPending: isUpdatingWorkspace } =
+    useUpdateWorkspace();
+  const { mutate: removeWorkSpace, isPending: isRemovingWorkspace } =
+    useRemoveWorkspace();
+
+  const handleEdit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    updateWorkSpace(
+      {
+        id: workspaceId,
+        name: value,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Workspace updated successfully");
+          setEditOpen(false);
+        },
+        onError: () => {
+          toast.error("Failed to update Workspace");
+        },
+      }
+    );
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+        <DialogHeader className="p-4 border-b bg-white">
+          <DialogTitle>{value}</DialogTitle>
+        </DialogHeader>
+        <div className="px-4 pb-4 flex-col gap-y-2">
+          <Dialog open={editOpen} onOpenChange={setEditOpen}>
+            <DialogTrigger asChild>
+              <div className=" px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                <div className="flex items-center justify-between">
+                  <p className=" text-sm font-semibold">Workspace Name</p>
+                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                    Edit
+                  </p>
+                </div>
+                <p className="text-sm">{value}</p>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Rename This Workspace</DialogTitle>
+              </DialogHeader>
+              <form className="space-y-4" onSubmit={handleEdit}>
+                <Input
+                  value={value}
+                  disabled={isUpdatingWorkspace}
+                  onChange={(e) => setValue(e.target.value)}
+                  required
+                  autoFocus
+                  minLength={3}
+                  maxLength={80}
+                  placeholder="Workspace name e.g. 'Work', 'Personal', 'Home' "
+                />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline" disabled={isUpdatingWorkspace}>
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button disabled={isUpdatingWorkspace}>Save</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
+          <button
+            disabled={false}
+            onClick={() => {}}
+            className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
+          >
+            <TrashIcon className="size-4" />
+            <p className="text-sm font-semibold">Delete Workspace</p>
+          </button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
