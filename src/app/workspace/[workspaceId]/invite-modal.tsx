285d041e1a33620606e2ca02e92b5551e896b82f
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-09 13:25:45 +0300

    changes

--- src/app/workspace/[workspaceId]/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { CopyIcon, RefreshCcw } from "lucide-react";
+import { useWorkspaceId } from "@/hooks/use-worksapce-id";
+import { toast } from "sonner";
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface InviteModalProps {
   open: boolean;
@@ -20,19 +27,75 @@ export const InviteModal = ({
   joinCode,
   name,
 }: InviteModalProps) => {
+  const { mutate, isPending } = useNewJoinCode();
+
+  const workspaceId = useWorkspaceId();
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This will deactivate the current invite code and generate a new one"
+  );
+
+  const handleNewCode = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    mutate(
+      { workspaceId },
+      {
+        onSuccess: () => {
+          toast.success("Invite code generated");
+        },
+        onError: () => {
+          toast.error("Failed to regenerate invite code ");
+        },
+      }
+    );
+  };
+
+  const handleCopy = () => {
+    const inviteLnk = `${window.location.origin}/join/${workspaceId}`;
+
+    navigator.clipboard
+      .writeText(inviteLnk)
+      .then(() => toast.success("Invite link copied to clipboard"));
+  };
+
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite people to {name}</DialogTitle>
-          <DialogDescription>
-            Use the code bellow to invite people to your workspace
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col gap-y-4 items-center justify-center py-10">
-          <p>{joinCode}</p>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite people to {name}</DialogTitle>
+            <DialogDescription>
+              Use the code bellow to invite people to your workspace
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+            <p className="text-4xl font-bold tracking-widest uppercase">
+              {joinCode}
+            </p>
+            <Button variant="ghost" size="sm" onClick={handleCopy}>
+              Copy link
+              <CopyIcon className="size-4 ml-2" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isPending}
+              onClick={handleNewCode}
+              variant="outline"
+            >
+              New Code
+              <RefreshCcw className="size-4 ml-2" />
+            </Button>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
