4002cbf4e2b1404bb3570ab65b0d74d4ed179235
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-09 17:29:39 +0300

    changes

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,22 +1,73 @@
 "use client";
 import Image from "next/image";
 import VerificationInput from "react-verification-input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useWorkspaceId } from "@/hooks/use-worksapce-id";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+import { Loader } from "lucide-react";
+import { useJoin } from "@/features/workspaces/api/use-join";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
-const JoinPage = () => {
+interface JoinPageProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const JoinPage = ({ params }: JoinPageProps) => {
+  const workspaceId = useWorkspaceId();
+
+  const router = useRouter();
+
+  const { mutate, isPending } = useJoin();
+
+  const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+
+  const handleComplete = (value: string) => {
+    mutate(
+      { workspaceId, joinCode: value },
+      {
+        onSuccess: (id) => {
+          router.replace(`/workspace/${id}`);
+          toast.success("Workspace joined successfully!");
+        },
+        onError: () => {
+          toast.success("Failed to join workspace");
+        },
+      }
+    );
+  };
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
   return (
     <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md ">
       <Image src="/logo.png" width={60} height={60} alt="logo" />
 
       <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
-          <h1 className="text-2xl font-bold">Join workspace</h1>
+          <h1 className="text-2xl font-bold">Join {data?.name}</h1>
           <p className="text-md text-muted-foreground">
             Enter the workspace code to join
           </p>
         </div>
         <VerificationInput
+          onComplete={handleComplete}
+          length={6}
           classNames={{
-            container: "flex gap-x-2",
+            container: cn(
+              "flex gap-x-2",
+              isPending && "opacity-50 cursor-not-allowed"
+            ),
             character:
               "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-500",
             characterInactive: "bg-muted",
@@ -24,6 +75,11 @@ const JoinPage = () => {
             characterFilled: "bg-white text-black",
           }}
         />
+      </div>
+      <div className="flex gap-x-4">
+        <Button size="lg" variant="outline" asChild>
+          <Link href="/"> Back to Home</Link>
+        </Button>
       </div>
     </div>
   );
