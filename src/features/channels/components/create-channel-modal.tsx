226ddd1dec19f12a100b43c41665c801d46cf98a
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-10 18:05:48 +0300

    update create channel component

--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -9,9 +9,11 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useCreateChannel } from "@/features/channels/api/use-create-channel";
-import { useWorkspaceId } from "@/hooks/use-worksapce-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
 
 export const CreateChannelModal = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
   const [open, setOpen] = useCreteChannelModal();
 
@@ -38,6 +40,7 @@ export const CreateChannelModal = () => {
       },
       {
         onSuccess: (id) => {
+          router.push(`/workspace/${workspaceId}/channel/${id}`);
           handleClose();
         },
       }
