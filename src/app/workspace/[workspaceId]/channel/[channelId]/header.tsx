06bf89a9227a877f62a8e7afe53c3a52df3432dd
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-11 11:02:15 +0300

    create delete channel method

--- src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { TrashIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 interface HeaderProps {
   title: string;
 }
 
 export const Header = ({ title }: HeaderProps) => {
+  const [editOpen, setEditOpen] = useState(false);
+  const [value, setValue] = useState(title);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+    setValue(value);
+  };
+
   return (
     <div className="bg-white border-b h-[49px] flex items-center px4 overflow-hidden">
       <Dialog>
@@ -32,14 +44,48 @@ export const Header = ({ title }: HeaderProps) => {
             <DialogTitle>#{title}</DialogTitle>
           </DialogHeader>
           <div className="px-4 pb-4 flex flex-col gap-y-2">
-            <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-              <div className=" flex items-center justify-between">
-                <p className="text-sm font-semibold">Channel Name</p>
-                <p className="text-sm text-[#1264a3] hover:underline font-semibold">
-                  Edit
-                </p>
-              </div>
-            </div>
+            <Dialog open={editOpen} onOpenChange={setEditOpen}>
+              <DialogTrigger asChild>
+                <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                  <div className=" flex items-center justify-between">
+                    <p className="text-sm font-semibold">Channel Name</p>
+                    <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                      Edit
+                    </p>
+                  </div>
+                  <p className="text-sm"># {title}</p>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogTitle>Rename this channel</DialogTitle>
+                <DialogHeader>
+                  <form className="space-y-4">
+                    <Input
+                      value={value}
+                      disabled={false}
+                      onChange={handleChange}
+                      required
+                      autoFocus
+                      minLength={3}
+                      maxLength={80}
+                      placeholder="e.g plab-budget"
+                    />
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="outline" disabled={false}>
+                          Cancel
+                        </Button>
+                      </DialogClose>
+                      <Button disabled={false}>Save</Button>
+                    </DialogFooter>
+                  </form>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+            <button className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600">
+              <TrashIcon className="size-4" />
+              <p className="text-sm font-semibold">Delete Channel</p>
+            </button>
           </div>
         </DialogContent>
       </Dialog>
