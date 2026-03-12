782e52a338a95079eec71c68d3cafb9077d612b5
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-12 09:53:52 +0300

    editor page

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat.input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat.input.tsx
@@ -1,11 +1,24 @@
 import dynamic from "next/dynamic";
+import { useRef } from "react";
+import Quill from "quill";
+
+interface ChatInputProps {
+  placeholder?: string;
+}
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
-export const ChatInput = () => {
+export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const editorRef = useRef<Quill | null>(null);
+
   return (
     <div className="px-5 w-full">
-      <Editor variant="update" />
+      <Editor
+        placeholder={placeholder}
+        onSubmit={() => {}}
+        disabled={false}
+        innerRef={editorRef}
+      />
       <div></div>
     </div>
   );
