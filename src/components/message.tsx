bb610201873f310341eea93328ab7730f5a7b1f2
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-19 17:44:24 +0300

    messages sending ui

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -35,7 +35,7 @@ const formatFullTime = (date: Date) => {
   return `${isToday(date) ? "Today" : isYesterday(date) ? "Yesterday" : format(date, "MMM d, yyyy")} at ${format(date, "h:mm:ss a")}`;
 };
 
-export const Message = ({
+const Message = ({
   id,
   isAuthor,
   memberId,
@@ -63,22 +63,50 @@ export const Message = ({
               {format(new Date(createdAt), "hh:mm")}
             </button>
           </Hint>
+          <div className="flex flex-col w-full">
+            <Renderer value={body} />
+          </div>
         </div>
-        <Renderer value={body} />
       </div>
     );
   }
+
+  const avatarFallback = authorName.charAt(0).toUpperCase();
+
   return (
     <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
       <div className="flex items-start gap-2">
         <button>
-          <Avatar>
-            <AvatarImage src={authorImage} />
-            <AvatarFallback></AvatarFallback>
+          <Avatar className="size-5 rounded-md mr-1">
+            <AvatarImage className="rounded-md" src={authorImage} />
+            <AvatarFallback className=" rounded-md bg-sky-500 text-white text-xs">
+              {avatarFallback}
+            </AvatarFallback>
           </Avatar>
         </button>
       </div>
-      <Renderer value={body} />
+      <div className="flex flex-col w-full overflow-hidden">
+        <div className="text-sm">
+          <button
+            onClick={() => {}}
+            className="font-bold text-primary hover:underline"
+          >
+            {authorName}
+          </button>
+          <span>&nbsp;&nbsp;</span>
+
+          <Hint label={formatFullTime(new Date(createdAt))}>
+            <button className="text-xs text-muted-foreground hover:underline">
+              {format(new Date(createdAt), "h:mm, a")}
+            </button>
+          </Hint>
+        </div>
+        <Renderer value={body} />
+        {updatedAt ? (
+          <span className="text-xs text-muted-foreground">(edited)</span>
+        ) : null}
+      </div>
     </div>
   );
 };
+export default Message;
