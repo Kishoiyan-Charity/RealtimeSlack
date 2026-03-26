091b9f7f5b677bf291b2894a82e6f63d0c934a8b
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-26 17:53:53 +0300

    messages and reactions

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -7,6 +7,8 @@ import { Id } from "../../convex/_generated/dataModel";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { Message } from "@/components/message";
+import { Loader } from "lucide-react";
+import entry from "next/dist/server/typescript/rules/entry";
 
 const TIME_THRESHOLD = 5;
 
@@ -17,7 +19,7 @@ interface MessageListProps {
   channelCreationTime?: number;
   variant?: "channel" | "thread" | "conversation";
   data: GetMessagesReturnType | undefined;
-  loadMore?: () => void;
+  loadMore: () => void;
   isLoadingMore: boolean;
   canLoadMore: boolean;
 }
@@ -107,6 +109,34 @@ export const MessageList = ({
           })}
         </div>
       ))}
+      {/*<div>*/}
+      {/*  <button onClick={loadMore} className="text-sm">*/}
+      {/*    Load More...*/}
+      {/*  </button>*/}
+      {/*</div>*/}
+      <div
+        className="h-1"
+        ref={(el) => {
+          const observer = new IntersectionObserver(
+            ([entry]) => {
+              if (entry.isIntersecting && canLoadMore) {
+                loadMore();
+              }
+            },
+            { threshold: 1.0 }
+          );
+          observer.observe(el);
+          return () => observer.disconnect();
+        }}
+      />
+      {isLoadingMore && (
+        <div className="text-center my-2 relative">
+          <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+          <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm ">
+            <Loader className="size-4 animate-spin" />
+          </span>
+        </div>
+      )}
       {variant === "channel" && channelName && channelCreationTime && (
         <ChannelHero name={channelName} creationTime={channelCreationTime} />
       )}
