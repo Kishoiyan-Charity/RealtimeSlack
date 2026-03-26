1b1a4cd96408446e30271d1eec7bb6e6112f342d
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-26 17:54:17 +0300

    messages and reactions ,loading

---
+++ src/features/messages/components/thread.tsx
@@ -0,0 +1,60 @@
+import { Id } from "../../../../convex/_generated/dataModel";
+import { Button } from "@/components/ui/button";
+import { AlertTriangle, Loader, XIcon } from "lucide-react";
+import { useGetMessage } from "@/features/messages/api/use-get-message";
+
+interface ThreadProps {
+  messageId: Id<"messages">;
+  onClose: () => void;
+}
+
+export const Thread = ({ messageId, onClose }: ThreadProps) => {
+  const { data: message, isLoading: loadingMessage } = useGetMessage({
+    id: messageId,
+  });
+
+  if (loadingMessage) {
+    return (
+      <div className="h-full flex flex-col">
+        <div className="h-[49px] flex justify-between items-center px-4 border-b">
+          <p className=" text-lg font-bold">Thread</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex flex-col gap-y-2 h-full items-center justify-center">
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </div>
+      </div>
+    );
+  }
+
+  if (!message) {
+    return (
+      <div className="h-full flex flex-col">
+        <div className="h-[49px] flex justify-between items-center px-4 border-b">
+          <p className=" text-lg font-bold">Thread</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex flex-col gap-y-2 h-full items-center justify-center">
+          <AlertTriangle className="size-5 animate-spin text-muted-foreground" />
+          <p className="text-sm text-muted-foreground">Message not found</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-full flex flex-col">
+      <div className="h-[49px] flex justify-between items-center px-4 border-b">
+        <p className=" text-lg font-bold">Thread</p>
+        <Button onClick={onClose} size="iconSm" variant="ghost">
+          <XIcon className="size-5 stroke-[1.5]" />
+        </Button>
+      </div>
+      <div>{JSON.stringify(message)}</div>
+    </div>
+  );
+};
