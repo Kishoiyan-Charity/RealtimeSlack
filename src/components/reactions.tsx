b5f20308d2d8e4ee86232c45a0ea6851628c4bfa
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-03-26 12:08:40 +0300

    messages and reactions

---
+++ src/components/reactions.tsx
@@ -0,0 +1,32 @@
+import { Doc, Id } from "../../convex/_generated/dataModel";
+import data from "@emoji-mart/data";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+
+interface ReactionsProps {
+  data: Array<
+    Omit<Doc<"reactions">, "memberId"> & {
+      count: number;
+      memberIds: Id<"members">[];
+    }
+  >;
+  onChange: (value: string) => void;
+}
+export const Reactions = ({ data, onChange }: ReactionsProps) => {
+  const workspaceId = useWorkspaceId();
+  const { data: currentMember } = useCurrentMember({ workspaceId });
+
+  const currentMemberId = currentMember?._id;
+
+  if (data.length === 0 || !currentMemberId) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center gap-1 mt-1 mb-1">
+      {data.map((reaction) => (
+        <button>{reaction.value}</button>
+      ))}
+    </div>
+  );
+};
