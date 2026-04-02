b043d66637c7d8f015c8ece0f3d160af9f47110d
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-04-02 14:29:11 +0300

    message editing and thread reply

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -2,10 +2,44 @@ import { Button } from "@/components/ui/button";
 import { Info, Search } from "lucide-react";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { useState } from "react";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { channel } from "node:diagnostics_channel";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const Toolbar = () => {
   const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data } = useGetWorkspace({ id: workspaceId });
+  const [open, setOpen] = useState(false);
+
+  const { data: channels } = useGetChannels({ workspaceId });
+  const { data: members } = useGetMembers({ workspaceId });
+
+  const onChannelClick = (channelId: string) => {
+    setOpen(false);
+
+    router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+  };
+
+  const onMemberClick = (memberId: string) => {
+    setOpen(false);
+
+    router.push(`/workspace/${workspaceId}/member/${memberId}`);
+  };
 
   return (
     <nav className="bg-[#481349] flex items-center justify-between h-10 p-1.5">
@@ -14,10 +48,41 @@ export const Toolbar = () => {
         <Button
           size="sm"
           className="bg-accent/25 hover:bg-accent/25 w-full justify-start h-7 px-2"
+          onClick={() => setOpen(true)}
         >
           <Search className="size-4 text-white mr-2" />
           <span className="text-white text-sm">Search {data?.name}</span>
         </Button>
+
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <Command>
+            <CommandInput placeholder="Type a command or search..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Channels">
+                {channels?.map((channel) => (
+                  <CommandItem
+                    onSelect={() => onChannelClick(channel._id)}
+                    key={channel._id}
+                  >
+                    {channel.name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+              <CommandSeparator />
+              <CommandGroup heading="Members">
+                {members?.map((member) => (
+                  <CommandItem
+                    onSelect={() => onMemberClick(member._id)}
+                    key={member._id}
+                  >
+                    {member.user.name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </CommandDialog>
       </div>
       <div className="ml-auto flex-1 flex items-center justify-end">
         <Button variant="transparent" size="iconSm">
